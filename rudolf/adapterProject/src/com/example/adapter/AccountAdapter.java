226843cdf933d2acb84e0496e66cb6f43711076d
rudikos <32672638+rudikos@users.noreply.github.com>
2017-12-21 17:55:50 +0400

    Update AccountAdapter.java

--- rudolf/adapterProject/src/com/example/adapter/AccountAdapter.java
+++ rudolf/adapterProject/src/com/example/adapter/AccountAdapter.java
@@ -7,25 +7,30 @@ import com.example.domain.Account;
  * @author rudolf.shakhgaldyan on 12/18/2017
  */
 public class AccountAdapter extends Account {
-
+    private double balance;
 
     private OffshoreAccount offshoreAccount;
 
     public AccountAdapter(OffshoreAccount offshoreAccount) {
         super(offshoreAccount.getOffshoreBalance());
-
-        // holds adaptee reference
         this.offshoreAccount = offshoreAccount;
-    }
 
-
-    @Override
-    public double getBalance() {
         double taxRate = offshoreAccount.getTaxRate();
         double grossBalance = offshoreAccount.getOffshoreBalance();
 
         double taxableBalance = grossBalance * taxRate;
         double balanceAfterTax = grossBalance - taxableBalance;
-        return balanceAfterTax;
+        this.balance = balanceAfterTax;
+
+    }
+
+    @Override
+    public double getBalance() {
+        return balance;
+    }
+
+    @Override
+    public boolean isOverdraftAvailable() {
+        return !super.isOverdraftAvailable();
     }
 }
